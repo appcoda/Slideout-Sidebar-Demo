69b7d08743eee81592291e6e4513c23509b39fc0
Simon Ng <simon@Simons-MacBook-Pro.local>
2015-01-05 01:04:47 +0800

    Initial version

---
+++ SidebarDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SidebarDemo
+//
+//  Created by Simon Ng on 9/11/14.
+//  Copyright (c) 2014 AppCoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
